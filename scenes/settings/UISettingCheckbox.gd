df6fad83fc1afe1b5fffe92cd8e3bd5721adc45f
Daniel Oaks <daniel@danieloaks.net>
2023-10-20 20:53:24 +1000

    Fix settings menu on an export

--- scenes/settings/UISettingCheckbox.gd
+++ scenes/settings/UISettingCheckbox.gd
@@ -1,7 +1,5 @@
-@tool
 extends HBoxContainer
 
-@onready var label = $Label
 @onready var checkbox = $CheckBox
 
 # set default
@@ -13,14 +11,3 @@ signal toggled(is_button_pressed)
 
 func _on_check_box_toggled(button_pressed: bool) -> void:
 	emit_signal("toggled", button_pressed)
-
-# pass through title
-@export var title: String # :
-#	set (value):
-#		print_debug("Title to set is: ", value)
-#
-#		if not is_node_ready():
-#			await ready
-#
-#		label.text = value
-#		title = value
--- scenes/settings/UISettingSlider.gd
+++ scenes/settings/UISettingSlider.gd
@@ -1,7 +1,5 @@
-@tool
 extends HBoxContainer
 
-@onready var label = $Label
 @onready var slider = $HSlider
 @onready var show_value = $ShowValue
 
@@ -16,52 +14,3 @@ signal value_changed(value)
 func _on_value_changed(value):
 	emit_signal("value_changed", value)
 	show_value.text = str(value)
-
-# pass through title
-@export var title := "" :
-	get:
-		return title
-
-	set (value):
-		# Wait until the scene is ready if `label` is null.
-		if not label:
-			await ready
-
-		label.text = value
-		title = value
-
-@export var min_value: float :
-	get:
-		return min_value
-	set(value):
-		if not slider:
-			await ready
-		slider.min_value = value
-		min_value = value
-
-@export var max_value: float :
-	get:
-		return max_value
-	set(value):
-		if not slider:
-			await ready
-		slider.max_value = value
-		max_value = value
-
-@export var step: float :
-	get:
-		return step
-	set(value):
-		if not slider:
-			await ready
-		slider.step = value
-		step = value
-
-@export var rounded: bool :
-	get:
-		return rounded
-	set(value):
-		if not slider:
-			await ready
-		slider.rounded = value
-		rounded = value
