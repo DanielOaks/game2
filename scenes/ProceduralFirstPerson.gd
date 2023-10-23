b42ec677fbc0d5ff379a7ed4ee4a8d86711037c4
Daniel Oaks <daniel@danieloaks.net>
2023-10-23 23:07:55 +1000

    Add counter to procedural level

--- scenes/ProceduralFirstPerson.gd
+++ scenes/ProceduralFirstPerson.gd
@@ -8,6 +8,8 @@ extends Node3D
 @onready var next_platform = $platforms/NextPlatform
 @onready var next_platform_material: StandardMaterial3D = $platforms/NextPlatform/Cylinder.get_active_material(0)
 
+var times_dropped_down: int = 0
+
 # see https://github.com/godotengine/godot-proposals/issues/1731
 # maybe this can be done nicer in the future :pray: :hands-raised:
 func new_direction() -> Vector2:
@@ -19,6 +21,12 @@ func new_direction() -> Vector2:
 func _ready():
 	current_platform_material.albedo_color = Color.from_hsv(randf(), .5, .7)
 	next_platform_material.albedo_color = Color.from_hsv(randf(), .5, .7)
+	
+	times_dropped_down = 0
+
+func _process(_delta):
+	if game_data.should_reset_game():
+		get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
 
 func _on_next_platform_body_entered(body):
 	if body != $Player:
@@ -40,3 +48,7 @@ func place_next_platform():
 	var new_dir := new_direction()
 	next_platform.position.x += new_dir.x * 12
 	next_platform.position.z += new_dir.y * 12
+	
+	# update counter
+	times_dropped_down += 1
+	$UI/TimesDroppedDown.text = str(times_dropped_down)
